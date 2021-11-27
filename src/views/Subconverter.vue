5912035a9afd6c08759627ec1bc4c0b9f45417cd
sh2yz <91233882+sh2yz@users.noreply.github.com>
2021-11-27 12:28:13 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -255,7 +255,7 @@ export default {
           ClashR: "clashr",
           Surge2: "surge&ver=2",
         },
-        backendOptions: [{ value: "https://sub.yz-sh.pp.ua/sub?" }],
+        backendOptions: [{ value: "https://sub.yz-sh.workers.dev/sub?" }],
         remoteConfig: [
         {
             label: "ACL4SSR",
